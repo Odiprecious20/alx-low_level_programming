78984771b98c77cd9a4f0f90145a5766c1a95235
Precious <odiprecious1@gmail.com>
2022-09-19 16:01:08 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps values of two integgers
+ * @a: is an integer
+ * @b: is an integer
+ * Return: no return
+ */
+void swap_int(int *a, int *b)
+{
+int c = *a;
+*a = *b;
+*b = c;
+}
