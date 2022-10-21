aeb1bc880a7eca93525abdda7ef0afdbc6d83df9
Precious <odiprecious1@gmail.com>
2022-10-20 20:48:33 -0700

    stringed lists

--- lists.h
+++ lists.h
@@ -6,6 +6,7 @@
 *	for all functions and types written in this proj
 */
 
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -24,6 +25,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
